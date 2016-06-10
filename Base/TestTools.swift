4f2c9082abc387f893b1e63a9205e80e57b4d821
shenhualxt <shenhualxt@163.com>
2016-06-10 17:38:34 +0800

    init project

---
+++ Base/TestTools.swift
@@ -0,0 +1,25 @@
+//
+//  TestTools.swift
+//  Licaishi_Framework
+//
+//  Created by Terry on 16/5/22.
+//  Copyright © 2016年 sina. All rights reserved.
+//
+
+import UIKit
+
+public class TestTools: NSObject {
+    
+    public func test2() ->Void{
+        print("test_in_Licaishi_framework");
+        
+    }
+
+}
+
+
+public func test() ->Void{
+    print("test_in_framework");
+    
+    
+}
